5ca4db53b75182ff0b2c64767c783a0b33bf0187
Felipe Oliveira <lippe637@gmail.com>
2025-07-19 14:44:11 -0300

    feat(backend): adiciona e ajusta modelos Pydantic

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,17 @@
- 
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class ReportBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class ReportCreate(ReportBase):
+    pass
+
+class Report(ReportBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True 
